e989fb75ea709f6217d323786ee579269867e377
Sergey Kurochkin <sergey.kurochkin@uptick.com>
2017-01-16 18:24:26 +0300

    change postinstall

---
+++ scripts/postinstall.js
@@ -0,0 +1,13 @@
+let sys = require('sys');
+let exec = require('child_process').exec;
+let os = require('os');
+
+if (os.platform === 'darwin') {
+    exec("unzip ../ios/libs/TwilioSDK/Libraries/libTwilioClient.a.zip -d ../ios/libs/TwilioSDK/Libraries/", function(err, stdout, stderr) {
+        sys.print('stdout: ' + stdout);
+        sys.print('stderr: ' + stderr);
+        if (err !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
+}
