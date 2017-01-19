42a998045916c0a71123f41324425a0c873ddcb8
Sergez <sichirc@gmail.com>
2017-01-19 14:54:09 +0300

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,8 @@
 let exec = require('child_process').exec;
 let os = require('os');
 
-console.log(os.platform);
-if (os.platform === 'darwin') {
+console.log(os.platform());
+if (os.platform() === 'darwin') {
     exec("unzip ../ios/libs/TwilioSDK/Libraries/libTwilioClient.a.zip -d ../ios/libs/TwilioSDK/Libraries/", function(err, stdout, stderr) {
         if (err !== null) {
             console.log('exec error: ' + error);
