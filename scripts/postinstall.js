2bdd96675281f924d6156905c8df6b33e8ef70e8
Sergez <sichirc@gmail.com>
2017-01-19 14:42:14 +0300

    remove deprecated sys

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,9 @@
-let sys = require('sys');
 let exec = require('child_process').exec;
 let os = require('os');
 
 console.log(os.platform);
 if (os.platform === 'darwin') {
     exec("unzip ../ios/libs/TwilioSDK/Libraries/libTwilioClient.a.zip -d ../ios/libs/TwilioSDK/Libraries/", function(err, stdout, stderr) {
-        sys.print('stdout: ' + stdout);
-        sys.print('stderr: ' + stderr);
         if (err !== null) {
             console.log('exec error: ' + error);
         }
