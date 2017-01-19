e1fb0c6c79249603be36b5c5fab0af91846acf84
Sergez <sichirc@gmail.com>
2017-01-19 14:57:08 +0300

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,7 +5,7 @@ console.log(os.platform());
 if (os.platform() === 'darwin') {
     exec("unzip ../ios/libs/TwilioSDK/Libraries/libTwilioClient.a.zip -d ../ios/libs/TwilioSDK/Libraries/", function(err, stdout, stderr) {
         if (err !== null) {
-            console.log('exec error: ' + error);
+            console.log('exec error: ' + err);
         }
     });
 }
